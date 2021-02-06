a5aaf84b369e4f1a5bdcd39eb837f0c1229d4ae4
Aparna Dinesh <adineshshylaja@unomaha.edu>
2021-02-06 01:56:29 -0600

    Update Gymmember

--- grm/migrations/0001_initial.py
+++ grm/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Customer',
+            name='Gymmember',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('cust_name', models.CharField(max_length=50)),
